c5ec4c5cfa234f1e949d7c695b9d1d1991b833cc
Timeth Subasinghe <timethsws@gmail.com>
2018-04-01 11:12:51 +0530

    Initial commit

---
+++ app/src/main/java/team/d3struct/smartmat/SplashScreen.java
@@ -0,0 +1,34 @@
+package team.d3struct.smartmat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashScreen extends AppCompatActivity {
+
+    //BluetoothServ bluetoothServ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(1000);
+                    //bluetoothServ.bluetooth();
+                    Intent intent = new Intent(getApplicationContext(),Home.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
