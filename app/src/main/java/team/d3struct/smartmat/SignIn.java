72cf22ccbe685ae15f3679f9efde0a542b2e2b27
Aysha Arafath <manalarafath@gmail.com>
2018-04-03 14:34:25 +0530

    sign in and sign up done

--- app/src/main/java/team/d3struct/smartmat/SignIn.java
+++ app/src/main/java/team/d3struct/smartmat/SignIn.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,8 +36,8 @@ public class SignIn extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        txt_email = (EditText)findViewById(R.id.txt_email);
-        txt_password = (EditText)findViewById(R.id.txt_password);
+        txt_email = (EditText)findViewById(R.id.txt_username);
+        txt_password = (EditText)findViewById(R.id.txt_email);
         btn_SignIn = (Button)findViewById(R.id.btn_SignIn);
         //btn_SignUp = (Button)findViewById(R.id.btn_SignUp);
 
